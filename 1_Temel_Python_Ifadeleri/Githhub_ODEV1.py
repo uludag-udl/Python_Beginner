7bd9860f19b75fc694e5bcda68ed9aea7fa56883
Uludağ Yapay Zeka Topluluğu <56990250+uludag-udl@users.noreply.github.com>
2019-11-17 01:38:00 +0300

    Update Githhub_ODEV1.py

--- 1_Temel_Python_Ifadeleri/Githhub_ODEV1.py
+++ 1_Temel_Python_Ifadeleri/Githhub_ODEV1.py
@@ -1,1 +1,29 @@
-asd
+1-)Kullanıcıdan bir kelime alıp bu kelimeyi bir değişkene aktardıktan sonra yan yana 10 kere yazdırınız.
+
+
+
+
+2-)Kullanıcıdan aldığınız iki sayının çarpımı ile bölümlerinin arasındaki fark nedir?
+
+
+
+
+
+Listeler:
+3-) a-) "Uludag Üniversitesi Yapay Zeka Topluluğu" 'nu liste haline getirip "Yapay" kelimesinin indexini ekrana yazdırınız.
+    b-) Daha sonra oluşturduğunuz bu listeye "üniversite"den sonra "Bursa" kelimesini ekleyiniz.
+    c-) En sondaki kelimeyi bu listeden siliniz.
+    d-)Listedeki Kelimleri sondan başa doğru sıralayınız.
+
+    
+    
+Set:
+4-) "lorem ipsum dolor sit amet ipsum dolor dolor sit amet  " 
+    a-)cümlesindeki her harfin bir kere yazdığı kümeyi oluşturunuz.
+    b-)bu cümlenin uzunluğunu ekrana yazdırınız.
+
+
+  
+ Dict:
+5-)Bir sözlük oluşturup 4 farklı bilgiyi bu sözlükte depolayınız.(indexleri 1,2,3,4 olacak şekilde)
+
