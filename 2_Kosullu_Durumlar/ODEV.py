4ab7ace08748056930b5bd2e12e24de704344c96
Uludağ Yapay Zeka Topluluğu <56990250+uludag-udl@users.noreply.github.com>
2019-12-05 08:10:40 +0300

    Create ODEV.py

---
+++ 2_Kosullu_Durumlar/ODEV.py
@@ -0,0 +1,18 @@
+1-)Kullanıcıdan istenilen "Kullanıcı Adı" ve "Şifre" ile :
+  1.1-)Eğer Kullanıcı Adı ve Şifre aynı anda doğruysa "Giriş Başarılı" şeklinde ekrana yazdırınız.
+  1.2) Sadece kullanıcı adı doğruysa "Kullanıcı Adınız Doğru Fakat Şifreniz Eşleşmiyor" tarzında çıktı vermeli.
+  1.3) Sadece girilen şifre doğruysa "Şifre Eşleşti Fakat Bir Sorun Var." tarzında bir çıktı vermeli.
+  1.4) ikisinden birisi doğruysa "Bir şeyler yanlış gitti" tarzında bir çıktı vermeli.
+  1.5) Eğer her ikiside yanlış girilmişse "Giriş yapılamadı" tarzında çıktı vermeli.
+  
+ 
+ 
+
+2-)Kullanıcıdan istenilen "Kullanıcı Adı" ve "Şifre" ile :
+    2.1-) Kullanıcı adı ve şifre doğru olana kadar tekrar tekrar ekrana sorguları çıkaran kodu yazınız.
+         (While Döngüsü)
+    2.2-) Kaçıncı seferde doğru bilgilerin girildiğini ekrana yazdırınız.
+    
+    
+ 
+ 
