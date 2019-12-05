32e8786cccab27ba240b68cefc982bf0a3de7a55
Uludağ Yapay Zeka Topluluğu <56990250+uludag-udl@users.noreply.github.com>
2019-12-05 08:34:02 +0300

    Create ODEV.py

---
+++ 4_Hata_Yakalama/ODEV.py
@@ -0,0 +1,35 @@
+1-)Kullanıcı "q" değerini girmedikçe aldığınız her inputun sayı olduğuna dikkat ediniz aksi takdirde "ValueError" varsa ekrana 
+"Geçersiz sayı" yazdırınız ve her durumda sonuç olarak ekrana "Hello World" yazdırınız.(Kullanılabilir:While)
+
+
+
+
+
+2-)Kullanıcıdan "Kullanıcı adı" "Şifre" "Telefon Numarası" bilgilerini isteyiniz ve :
+  1.1-)Eğer Kullanıcı adı "int" tipinde girilmişse "Kullanıcı adı int olamaz" şeklinde bir çıktı,
+  1.2-)Eğer şifrenin tipi "str" ise "Şifreniz int olamaz" şeklinde bir çıktı,
+  
+
+
+
+
+3-)Girilen parola içinde Türkçe karakterler var ise TypeError ile "Türkçe Karakter Algılandı" şeklinde ekrana çıktı veriniz. 
+  (Yol_1 => For,if,raise komutları ile yapılabilir.)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  https://docs.python.org/3/tutorial/errors.html
+  
+  
+  
+  
